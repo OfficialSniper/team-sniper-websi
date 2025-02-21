7c72ee1de3b283dbc25cda7957e99d9a4e6a89b1
OfficialSniper <sniperportal89@gmail.com>
2025-02-21 20:37:35 +0530

    script.js

--- script.js
+++ script.js
@@ -15,4 +15,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    // Adding Fade-in effect on load
+    const sections = document.querySelectorAll(".fade-in");
+    sections.forEach((section, index) => {
+        section.style.animationDelay = `${index * 0.5}s`;
+    });
 });
